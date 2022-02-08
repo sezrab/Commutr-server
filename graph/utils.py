db3939fc9e583621a707b76aaf6d748ffeb61a65
Sam Berry <samuel.ezra.berry@gmail.com>
2022-02-08 10:05:56 +0000

    Update utils.py

--- graph/utils.py
+++ graph/utils.py
@@ -1,27 +1,48 @@
 class PriorityQueue(object):
     def __init__(self):
+        # PriorityQueue.__queue takes form [ [node (Node), fCost (float)], ... ] 
         self.__queue = []
-
-    def __str__(self):
-        return ' '.join([str(i) for i in self.__queue])
 
     @property
     def empty(self):
+        """Property that returns true if the queue has no items.
+
+        Returns:
+            bool: True if empty
+        """
         return len(self.__queue) == 0
 
     def contains(self,node):
+        """To check whether the queue contains a certian node
+
+        Args:
+            node (Node)
+
+        Returns:
+            bool: True if node is present in queue
+        """
         for item in self.__queue:
             if node in item:
                 return True
         return False
 
     def remove(self,node):
+        """For removing a node from the queue without dequeueing
+
+        Args:
+            node (Node): node to remove
+        """
         for item in self.__queue:
             if node in item:
                 self.__queue.remove(item)
-        return False
 
     def enqueue(self, node, f):
+        """Enqueue a node with a certain F cost.
+
+        Args:
+            node (Node): Node to enqueue
+            f (float): F cost of the node
+        """
         if self.empty:
             self.__queue.append([node,f])
         else:
@@ -32,7 +53,18 @@ class PriorityQueue(object):
             self.__queue.insert(i,[node,f])
   
     def dequeue(self):
+        """Dequeue the node with the least F cost
+
+        Returns:
+            Node
+        """
+        assert not self.empty
         return self.__queue.pop(0)[0]
     
     def __str__(self) -> str:
+        """String representation of PriorityQueue. For debugging
+
+        Returns:
+            string
+        """
         return str(list(map(lambda x: x[1], self.__queue)))
